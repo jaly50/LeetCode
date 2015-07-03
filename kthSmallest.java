49d9f5a43a9eab6781c39977b62fd31265ba3491
Jiali Chen <scarlett.chen50@gmail.com>
2015-07-02 23:21:27 -0400

    Update kthSmallest.java

--- kthSmallest.java
+++ kthSmallest.java
@@ -29,4 +29,27 @@ public class Solution {
         return 0;
         
     }
+    // Using morris. No stack. o1 space.
+        public int kthSmallest(TreeNode root, int k) {
+        int count=0;
+        while (root!=null) {
+            if (root.left==null) {
+                count++;
+                if (count==k) return root.val;
+                root = root.right;
+            }
+            else {
+               TreeNode left = root.left;
+               TreeNode temp = left;
+               while (left.right!=null) {
+                   left = left.right;
+               }
+               left.right = root;
+               root.left = null;
+               root = temp;
+            }
+        }
+        return 0;
+    }
+    
 }
