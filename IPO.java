894d41550bb07a7fed91c32d9a391b190bb080a8
Jiali Chen <scarlett.chen50@gmail.com>
2017-02-18 16:39:54 -0800

    Update IPO.java

--- IPO.java
+++ IPO.java
@@ -21,4 +21,31 @@ public class Solution {
         return W;
         
     }
+    
+        public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
+        // 首先找到all capital which smaller than current ones, then pick one with the most profit 
+        // Above:k*n Time Limit Exceeded 50000 data
+        // The other option: 用priority queue找到满足当前Capitial的最大profit
+        // priority Queue complexity: nlogn
+        PriorityQueue<int[]> pqCap = new PriorityQueue<>((a,b) -> (a[0] - b[0]));
+        PriorityQueue<int[]> pqPro = new PriorityQueue<>((a,b) -> (b[1] - a[1]));
+        int n = Capital.length;
+        for (int i=0; i<n; i++) pqCap.add(new int[]{Capital[i], Profits[i]});
+        for (int i=0; i<k; i++) {
+              while (!pqCap.isEmpty()) {
+                  int[] pair = pqCap.peek();
+                  if (pair[0] <= W) {
+                      pqPro.add(pqCap.poll());
+                  }
+                  else break;
+              }
+              if (!pqPro.isEmpty()) {
+                  int[] pair = pqPro.poll();
+                  W += pair[1];
+              }
+              else return W;
+        }
+        return W;
+        
+    }
 }
